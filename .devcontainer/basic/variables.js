70625c926962290ec23a5bcccf26b6adc036d245
HiteshAmbaliya7 <155156476+HiteshAmbaliya7@users.noreply.github.com>
2024-10-20 16:07:46 +0000

    variables

---
+++ .devcontainer/basic/variables.js
@@ -0,0 +1,33 @@
+// variable in javascript 
+const name="hitesh"
+console.log(name);
+//const not be change  and we will difine that type variable to unchane variable.
+let name1 ="Ambaliya"
+console.log(name1);
+// let type variable in genrel and most use type.
+var name2 ="ahir"
+console.log(name2);
+//this type variable not as must use but it some time issue in block and function structer.
+//alert(3+3);  dont work because 
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
