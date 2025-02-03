ce9cae301ca55343c8a7a5d62a28fa2fa156d175
HiteshAmbaliya7 <155156476+HiteshAmbaliya7@users.noreply.github.com>
2025-02-03 16:58:02 +0000

    array and methods_1

---
+++ arrays/array.js
@@ -0,0 +1,13 @@
+//Array
+const myArr=[0,1,2,3,4,5];
+const myHeros =["tony stark","hulk","caption amarika"];
+const Arr=new Array(1,2,3,4,5,6,7);
+
+//array methods 
+//myArr.push(6);//insert value
+//myArr.pop();// delet lastvale of array
+// myArr.unshift(9); // add vale at front
+myArr.shift();           // remove item at front
+
+
+console.log(myArr);
