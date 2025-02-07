2193303a73dd6ebae93610947b50c7f8ca65b63f
Hitesh Ambaliya <155156476+HiteshAmbaliya7@users.noreply.github.com>
2025-02-07 16:09:21 +0000

    object operations

--- objects/object_02.js
+++ objects/object_02.js
@@ -10,4 +10,26 @@ instauser.islogedin=false
 
 
 
- 
+const regularuser={
+    email :"mrhitesh@gmail.com",
+    fullname : {
+        username:{
+                firstname:"hitesh",
+                lastname:"ambaliya"
+        }
+    }
+ }
+
+
+ // assiccing values in nested objcts
+//  console.log(regularuser.fullname.username.firstname);
+
+// combinoing two objects 
+const obj1 ={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+
+// const obj3=Object.assign({},obj1,obj2)
+
+//  using spred oprator
+const obj3={...obj1,...obj2}
+console.log(obj3);
