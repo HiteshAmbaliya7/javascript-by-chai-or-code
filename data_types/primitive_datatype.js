d3735f414ff8fa36edd0fdf911eda1009a0a6904
Hitesh Ambaliya <155156476+HiteshAmbaliya7@users.noreply.github.com>
2025-02-09 18:12:37 +0000

    primitive datatypes

---
+++ data_types/primitive_datatype.js
@@ -0,0 +1,37 @@
+/*
+ DATA TYPES IN JAVASCRIPT :
+
+
+
+
+ In javascript Main 2 datatypes
+        -primitive datatypes
+        -non primitive datatypes
+
+
+
+
+
+
+ => PRIMITIVE DATA TYPES:
+        -String
+        -Boolean
+        -Number(int)
+        -null
+        -undefine
+        -Symbol
+        -BigInt
+*/
+const score =100
+const name="hitesh"
+const scorevalue =100.3
+const isLOggedIn =false
+const outsideTemp=null
+let userEmail;
+const id =Symbol('123')
+const anotherid=Symbol('123')
+console.log(id===anotherid); // output : false  
+const bigNumber=123456789987456123n
+
+
+
