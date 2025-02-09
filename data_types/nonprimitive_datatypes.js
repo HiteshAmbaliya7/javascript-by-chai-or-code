75acb15d191ce832b2c6f383e4affbed42b5753b
Hitesh Ambaliya <155156476+HiteshAmbaliya7@users.noreply.github.com>
2025-02-09 18:23:48 +0000

    non primitive datatypes

---
+++ data_types/nonprimitive_datatypes.js
@@ -0,0 +1,18 @@
+/*
+NONPRIMITIVE DATATYPES IN JAVASCRIPT
+Also known as reference datatypes
+
+    -Array
+    -Objects
+    -functions
+*/
+const myarr=[1,2,3,4,5,6,7];
+const myObj =  {
+    name : "hitesh",
+    age : 21,
+}
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}
